656f310ce9bd8d25921f988e10f3f37c15ed0ffd
Rizwan Mohamed Ibrahim <rizwankce@users.noreply.github.com>
2017-06-16 21:01:20 +0800

    [ADD] Custom camera with preview view

---
+++ Camera/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Camera
+//
+//  Created by Rizwan on 16/06/17.
+//  Copyright © 2017 Rizwan. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var previewView: UIView!
+    
+    var captureSession: AVCaptureSession?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Get an instance of the AVCaptureDevice class to initialize a device object and provide the video as the media type parameter
+        let captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        
+        do {
+            // Get an instance of the AVCaptureDeviceInput class using the previous deivce object
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            
+            // Initialize the captureSession object
+            captureSession = AVCaptureSession()
+            
+            // Set the input devcie on the capture session
+            captureSession?.addInput(input)
+            
+            //Initialise the video preview layer and add it as a sublayer to the viewPreview view's layer
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            videoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+            videoPreviewLayer?.frame = view.layer.bounds
+            previewView.layer.addSublayer(videoPreviewLayer!)
+            
+            //start video capture
+            captureSession?.startRunning()
+        } catch {
+            //If any error occurs, simply print it out
+            print(error)
+            return
+        }
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
